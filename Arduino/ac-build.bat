eef61249aae7652024ce541ccd4a23a87282ef21
jdfin <john.finley@gmail.com>
2022-12-10 14:48:46 -0600

    enable feather m4 and beetle only

--- Arduino/ac-build.bat
+++ Arduino/ac-build.bat
@@ -3,8 +3,15 @@ arduino-cli compile %1 ^
     --config-file ./config.yaml ^
     --fqbn %FQBN%
 
+::  --clean
+::  --verbose
+::  --build-path "%CD%/build"
+
 if "%2" == "" goto :eof
 
 arduino-cli upload %1 -p %2 ^
     --config-file ./config.yaml ^
     --fqbn %FQBN%
+
+::  --verbose
+::  --build-path "%CD%/build"
--- Arduino/ac-init.bat
+++ Arduino/ac-init.bat
@@ -16,33 +16,50 @@ arduino-cli config set directories.user "%cd%" ^
 
 ::::: Add Board Manager URLs
 
+::goto skip_adafruit
 arduino-cli config add board_manager.additional_urls ^
     https://adafruit.github.io/arduino-board-index/package_adafruit_index.json ^
     --config-file .\config.yaml
+:skip_adafruit
 
+goto skip_seeedstudio
 arduino-cli config add board_manager.additional_urls ^
     https://files.seeedstudio.com/arduino/package_seeeduino_boards_index.json ^
     --config-file .\config.yaml
+:skip_seeedstudio
 
-:: arduino-cli config add board_manager.additional_urls ^
-::     https://www.pjrc.com/teensy/package_teensy_index.json ^
-::     --config-file .\config.yaml
+goto skip_pjrc
+arduino-cli config add board_manager.additional_urls ^
+    https://www.pjrc.com/teensy/package_teensy_index.json ^
+    --config-file .\config.yaml
+:skip_pjrc
 
 ::::: Install Cores
 
-:: arduino-cli core install --no-overwrite arduino:avr ^
-::     --config-file .\config.yaml
-:: echo set FQBN=arduino:avr:leonardo > ac-beetle.bat
+arduino-cli core update-index ^
+    --config-file .\config.yaml
 
+::goto skip_arduino_avr
+arduino-cli core install --no-overwrite arduino:avr ^
+    --config-file .\config.yaml
+echo set FQBN=arduino:avr:leonardo > ac-beetle.bat
+:skip_arduino_avr
+
+::goto skip_adafruit_samd
 arduino-cli core install --no-overwrite adafruit:samd ^
     --config-file .\config.yaml
 :: echo set FQBN=adafruit:samd:adafruit_qtpy_m0 > ac-qtpy_m0.bat
 echo set FQBN=adafruit:samd:adafruit_feather_m4 > ac-feather_m4.bat
+:skip_adafruit_samd
 
+goto skip_seeeduino_samd
 arduino-cli core install --no-overwrite Seeeduino:samd ^
     --config-file .\config.yaml
 echo set FQBN=Seeeduino:samd:seeed_XIAO_m0 > ac-xiao.bat
+:skip_seeeduino_samd
 
-:: arduino-cli core install --no-overwrite teensy:avr ^
-::     --config-file .\config.yaml
-:: echo set FQBN=teensy:avr:teensy40 > ac-teensy.bat
+goto skip_teensy_avr
+arduino-cli core install --no-overwrite teensy:avr ^
+    --config-file .\config.yaml
+echo set FQBN=teensy:avr:teensy40 > ac-teensy.bat
+:skip_teensy_avr
